5797aaeba41a5f3fcf3a6df0ea17a2f2f540fca1
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2024-09-20 22:55:29 +0800

    Jumat, 20-09-2024 (DOM)

---
+++ Chapter-2/20-09-2024 (DOM)/self-learn/index.js
@@ -0,0 +1,4 @@
+const inputFirstName = document.getElementById("firstName");
+const inputLastName = document.getElementById("lastName");
+const inputEmail = document.getElementById("email");
+const btnSubmit = document.getElementById("submit");
