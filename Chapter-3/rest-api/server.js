563eddf9b6469263957dc56cb977ca6855f694eb
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2024-10-03 23:03:11 +0800

    get detail car data and error handling

--- Chapter-3/rest-api/server.js
+++ Chapter-3/rest-api/server.js
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
 
 // /api/(version)/(collection) => collection harus jamak(s)
 app.get("/api/v1/cars", (req, res) => {
+  // select * from  .....
   try {
     const cars = readJSON("cars.json");
     res.status(200).json({
@@ -40,7 +41,7 @@ app.get("/api/v1/cars", (req, res) => {
 
 // create
 app.post("/api/v1/cars", (req, res) => {
-  // tambahin try-catch
+  // insert into ...
   const cars = readJSON("cars.json");
   const newCar = req.body;
   cars.push(newCar);
@@ -60,12 +61,46 @@ app.post("/api/v1/cars", (req, res) => {
   );
 });
 
+app.get("/api/v1/cars/:id", (req, res) => {
+  // insert into ...
+  const idCar = req.params.id;
+  const cars = readJSON("cars.json");
+
+  // == tidak peduli tipe datanya apa, 10 = "10"
+  // === peduli tipe datanya apa, 10 != "10"
+
+  // changing type string to integer --> "10" * 1 = 10 (number)
+
+  // in cars's array, find object that contains id == idCar
+  const car = cars.find((object) => object.id === idCar);
+
+  // salah satu basic error handling
+  // cant find spesific car
+  if (!car) {
+    return res.status(404).json({
+      status: "failed",
+      message: `cant find spesific data id : ${idCar}`,
+      isSuccess: false,
+      data: null,
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "success get car data",
+    isSuccess: true,
+    data: {
+      car,
+    },
+  });
+});
+
 // if url doesnt exist, run this code below
 app.use((req, res, next) => {
   // status not found
   res.status(404).json({
     status: "failed",
-    message: "url not found !",
+    message: "API not found !",
   });
 });
 
